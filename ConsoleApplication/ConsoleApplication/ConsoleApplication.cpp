da447381d26da6476f50a87d61b81fcedde07613
Jozer <kmakismov@yandex.ru>
2024-02-10 11:05:37 +0500

    first commit

---
+++ ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool equal(int N, int S) {
+    if (N == 0)
+        return S == 0;
+    return equal(N / 10, S - N % 10);
+}
+
+int main() {
+    cout << boolalpha;
+    cout << equal(12345, 15) << endl; 
+    cout << equal(24, 7) << endl;     
+    cout << equal(100, 1) << endl;    
+
+    return 0;
+}
